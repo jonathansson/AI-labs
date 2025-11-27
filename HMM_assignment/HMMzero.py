242edb738f45cb2ef4d9dc1a54d8d92881385362
jonathansson <jonathansson98@gmail.com>
2025-11-27 22:02:38 +0100

    Matrix multiplication fixed

--- HMM_assignment/HMMzero.py
+++ HMM_assignment/HMMzero.py
@@ -19,23 +19,24 @@ def parse(file):
 
 # matrix a * marix b
 def matrix_mult(a, b):
-    x = 0
 
-    a_rows = a[0]
-    a_cols = a[1]
-    b_rows = b[0]
-    b_cols = b[1]
+    a_rows = int(a[0])
+    a_cols = int(a[1])
+    b_rows = int(b[0])
+    b_cols = int(b[1])
     res=[a_rows, b_cols]
 
-    while(x < int(a_cols)):
-        sum = 0
-        y = 0
-        while(y < (b_rows)):
-            print(y)
-            sum += a[y + 2] * b[int(x + (y * b_cols))]
-            y += 1
-        res.append(sum)
-        x += 1
+    # for each row in a
+    for i in range(a_rows):
+        # for each column in b
+        for j in range(b_cols):
+            sum = 0
+            # dot product of row i of a with column j of b
+            for k in range(a_cols):
+                element_a = a[2 + i * a_cols + k]  # element at row i, col k of matrix a
+                element_b = b[2 + k * b_cols + j]  # element at row k, col j of matrix b
+                sum += element_a * element_b
+            res.append(sum)
     
     return res
 
