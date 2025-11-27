ecc0a6a5c3051527650f75094939aa167b706981
jonathansson <jonathansson98@gmail.com>
2025-11-27 22:29:34 +0100

    more appropriate variable names 2.0

--- HMM_assignment/HMMzero.py
+++ HMM_assignment/HMMzero.py
@@ -45,5 +45,5 @@ def matrix_mult(a, b):
 transition, emission, initial = parse(file)
 init_tran = matrix_mult(initial, transition)
 print(init_tran)
-init_tran_emis = matrix_mult(gubbe, emission)
+init_tran_emis = matrix_mult(init_tran, emission)
 print(init_tran_emis)
